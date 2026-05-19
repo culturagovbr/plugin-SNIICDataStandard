5f12ff5e2619dc415bbe4ff6d52560d19a2bdda0
Lucas Oliveira <lucas.fullstack.dev@gmail.com>
2026-05-19 19:58:59 -0300

    initial commit

---
+++ Plugin.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace SNIICDataStandard;
+
+use MapasCulturais\App;
+
+class Plugin extends \MapasCulturais\Plugin
+{
+    public function _init() {}
+
+    public function register() {}
+}
