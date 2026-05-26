46019aa2d93272d55f36821dc2144f2715ca55c3
Lucas Oliveira <lucas.fullstack.dev@gmail.com>
2026-05-26 11:32:56 -0300

    fix: corrige inserção de Espaços e Equipamentos Culturais na taxonomia sem sort()

--- taxonomies.php
+++ taxonomies.php
@@ -4,7 +4,12 @@ use MapasCulturais\i;
 
 $taxonomies = include APPLICATION_PATH . '/conf/taxonomies.php';
 
-$taxonomies[2]['restricted_terms'][] = i::__('Espaços e Equipamentos Culturais');
-sort($taxonomies[2]['restricted_terms']);
+$terms = &$taxonomies[2]['restricted_terms'];
+$pos = array_search(i::__('Espetáculo de Circo'), $terms);
+if ($pos !== false) {
+    array_splice($terms, $pos, 0, [i::__('Espaços e Equipamentos Culturais')]);
+} else {
+    $terms[] = i::__('Espaços e Equipamentos Culturais');
+}
 
 return $taxonomies;
